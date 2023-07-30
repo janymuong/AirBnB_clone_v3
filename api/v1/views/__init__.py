7515b623f00be2ea8e0a69cc46dcabbba392b9eb
Jany Muong <me.roumuong@gmail.com>
2023-07-30 18:13:41 +0300

    [fix circular import in api/v1/views/__init__.py] return API status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ creates a Blueprint instance with `url_prefix` set to `/api/v1`
 register the routes for app_views
 '''
 from flask import Blueprint
-from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
