4018b4fc9f160f01420df7a7c46aad8d3fe809af
Jany Muong <me.roumuong@gmail.com>
2023-07-30 18:30:04 +0300

    fix environment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,6 @@ def teardown_engine(exception):
 
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
