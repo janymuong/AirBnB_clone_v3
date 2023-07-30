c517d67bd2f28b6a01ccf0e0ffd6481949447547
Jany Muong <me.roumuong@gmail.com>
2023-07-30 18:39:14 +0300

    [fix] API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,6 @@ def teardown_engine(exception):
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = getenv('HBNB_API_PORT', 5000)
+    app.run(host=HOST, port=PORT, threaded=True)
