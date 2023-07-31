d33a79b40cb17e6d24b447312b95022589a71efb
Jany Muong <me.roumuong@gmail.com>
2023-07-31 22:08:03 +0300

    view function for Users objects that handles all default `RESTFul API` actions

---
+++ api/v1/views/users.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+'''module api/v1/views/users.py:
+create a new view for User objects - handles all default RESTful API actions
+'''
+from flask import abort, jsonify, request
+
+from models.user import User
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_all_users():
+    '''GET /users
+    retrieves the list of all User objects
+    '''
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    '''GET /users/<user_id>
+    Retrieves a User object
+    '''
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    '''DELETE /users/<user_id>
+    Deletes a User object
+    '''
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    '''POST /users
+    Creates a User object
+    '''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    '''PUT /users/<user_id>
+    Updates a User object
+    '''
+    user = storage.get(User, user_id)
+    if user:
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+
+        data = request.get_json()
+        ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(user, key, value)
+
+        user.save()
+        return jsonify(user.to_dict()), 200
+    else:
+        abort(404)
+
+
+# Error Handlers:
+@app_views.errorhandler(404)
+def not_found(error):
+    '''404: Not Found'''
+    return jsonify({'error': 'Not found'}), 404
+
+
+@app_views.errorhandler(400)
+def bad_request(error):
+    '''400:
+    return Bad Request message for illegal requests to the API
+    '''
+    return jsonify({'error': 'Bad Request'}), 400
